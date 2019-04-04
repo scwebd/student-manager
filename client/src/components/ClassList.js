93a94c773ff970006b358c8189a7bded8d892a03
scwebd <scwebd@gmail.com>
2019-04-03 20:33:48 -0600

    Alphabetizing class list

--- client/src/components/ClassList.js
+++ client/src/components/ClassList.js
@@ -7,7 +7,7 @@ const ClassList = props => {
             <Label for="name" hidden>Choose Class</Label>
             <Input type="select" name="class" id="class" value={props.class} onChange={props.handleInputChange}>
                 <option>Choose class</option>
-                {props.classes.map(classId => (
+                {props.classes.sort().map(classId => (
                     <option value={classId}>{classId}</option>
                 ))}
             </Input>
